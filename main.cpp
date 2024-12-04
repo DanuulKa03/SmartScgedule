6ea48b030a5c507e14578c978639c5f25b9b7eab
DanuulKA_PC <daniil.kazakov@piklema.com>
2024-12-04 07:40:55 +0300

    EM-7: донастройка проекта и добавление картинок

--- main.cpp
+++ main.cpp
@@ -4,20 +4,42 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickStyle>
+#include <QDebug>
+#include <QtQml/qqmlextensionplugin.h>
 
 int main(int argc, char *argv[])
 {
+    // // Установка стиля для Qt Quick Controls
+    // QQuickStyle::setStyle("Material");
+
     QCoreApplication::setOrganizationName("QtProject");
-    QCoreApplication::setApplicationName("SmartScgedule");
+    QCoreApplication::setApplicationName("SmartSchedule");
 
     QGuiApplication app(argc, argv);
 
+    Q_IMPORT_QML_PLUGIN(CommonPlugin)
+    Q_IMPORT_QML_PLUGIN(PagesPlugin)
+    Q_IMPORT_QML_PLUGIN(ComponentsPlugin)
+    Q_IMPORT_QML_PLUGIN(FormsPlugin)
+
     QQmlApplicationEngine engine;
 
+    // Добавление путей для импорта QML
+    engine.addImportPath("qrc:/qt/qml");
+    engine.addImportPath("qrc:/qt/qml/UILibrary");
+
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-            &app, []() { QCoreApplication::exit(-1); },
-            Qt::QueuedConnection);
-    engine.loadFromModule("demos.smartscgedule", "Main");
+                     &app, []() { QCoreApplication::exit(-1); },
+                     Qt::QueuedConnection);
+
+    // Загрузка модуля QML
+    engine.loadFromModule("SmartSchedule", "Main");
+
+    // Проверка успешности загрузки QML
+    if (engine.rootObjects().isEmpty()) {
+        qCritical() << "Failed to load QML root objects.";
+        return -1;
+    }
 
     return app.exec();
 }
